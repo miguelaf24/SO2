f59553846eaccc10d466ba0edcbb6f9efe474bc0
bsantos01 <bdps2005@gmail.com>
2018-05-08 17:44:35 +0100

    shm

--- Space Invaders/Space Invaders/utils.h
+++ Space Invaders/Space Invaders/utils.h
@@ -7,6 +7,16 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
+#define BUFFSIZE 30
+
+
+
+//direções
+#define LEFT 0
+#define UP 1
+#define RIGHT 2
+#define DOWN 3
+
 struct Elemento {
 	int x;
 	int y;
@@ -27,14 +37,39 @@ typedef struct {
 typedef struct {
 	TCHAR name;
 	int efect;
+	struct Elemento e;
 }PowerUP;
 
 typedef struct {
 	Nave navesnormais[10];
 	Nave navesesquivas[10];
-}Jogo;
+	Player player1;
+	int dificuldade;
+}Jogo, *pJogo;
 
 typedef struct {
+	struct Elemento e;
+}Tiro;
+
+typedef struct {
+	struct Elemento e;
+}Bomba;
+
+typedef struct {
+	struct Elemento e;
+	int lifes;
+	int velocidade;
 	TCHAR name;
 	TCHAR points;
 }Player;
+
+typedef struct {
+	int id;
+	int Dir;
+}Jogada;
+
+typedef struct Buffer {
+	int nextIn;	//indice de escrita
+	int nextOut; //indice de leitura
+	Jogada buffer[BUFFSIZE];
+}Buff, *pBuff;
