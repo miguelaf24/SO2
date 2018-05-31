42fc55eab6ec2358d7e1261f8e8efd5853a0df39
bsantos01 <bdps2005@gmail.com>
2018-05-31 23:05:16 +0100

    piping started

---
+++ Space Invaders/Client/client.cpp
@@ -0,0 +1,129 @@
+#include "../Space Invaders/utils.h"
+
+
+#define PIPE_NAME TEXT("\\\\.\\pipe\\GamePipe")
+
+//VARIAVEIS GLOBAIS
+HANDLE hPipe;
+
+//THREADS
+DWORD WINAPI thread_write(LPVOID data);
+DWORD WINAPI thread_read(LPVOID data);
+
+
+//MAIN
+int _tmain(int argc, LPTSTR argv[]) {
+
+	int i = 0;
+
+
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+#endif
+	_tprintf(TEXT("[DEBUG] Esperar pelo pipe '%s' (WaitNamedPipe)\n"), PIPE_NAME);
+	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'!\n"), PIPE_NAME);
+		Sleep(2000);
+		exit(-1);
+	}
+	_tprintf(TEXT("[DEBUG] Ligação ao pipe do escritor... (CreateFile)\n"));
+	hPipe = CreateFile(PIPE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0 | FILE_FLAG_OVERLAPPED, NULL);
+	if (hPipe == NULL) {
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'! (CreateFile)\n"), PIPE_NAME);
+		Sleep(2000);
+		exit(-1);
+	}
+	_tprintf(TEXT("[DEBUG] Liguei-me...\n"));
+
+
+	//ALTERAR MODO DO PIPE
+	DWORD modo = PIPE_READMODE_MESSAGE;
+	SetNamedPipeHandleState(hPipe, &modo, NULL, NULL);
+
+	HANDLE htTreadw;
+	htTreadw = CreateThread(NULL, 0, thread_write, NULL, 0, NULL);
+	HANDLE htTreadr;
+	htTreadr = CreateThread(NULL, 0, thread_read, NULL, 0, NULL);
+
+
+	WaitForSingleObject(htTreadr, INFINITE);
+	WaitForSingleObject(htTreadw, INFINITE);
+	CloseHandle(htTreadw);
+	CloseHandle(htTreadr);
+	CloseHandle(hPipe);
+	Sleep(200);
+	return 0;
+}
+
+
+DWORD WINAPI thread_read(LPVOID data) {
+	Jogo j;
+	BOOL ret=false;
+	DWORD n;
+	HANDLE IOReady;
+	OVERLAPPED Ov;
+	IOReady = CreateEvent(NULL, TRUE, FALSE, NULL);
+	while (1) {
+		ZeroMemory(&Ov, sizeof(Ov));
+		ResetEvent(IOReady);
+		Ov.hEvent = IOReady;
+
+		ret = ReadFile(hPipe, &j, sizeof(Jogo), &n, &Ov);
+		WaitForSingleObject(IOReady, INFINITE);
+		 GetOverlappedResult(hPipe, &Ov, &n, FALSE);
+
+		
+	/*	if (!ret) {
+			_tprintf(TEXT("[ERROR] READING %d %d... threadRead \n"), ret, n);
+			continue;
+		}*/
+		if (n<sizeof(Jogo)) {
+			_tprintf(TEXT("[ERROR] Less bites received %d %d... (ReadFile)\n"), ret, n);
+			break;
+		}
+
+
+		_tprintf(TEXT("[DEBUG] Recebi %d bytes, jogodif: %d\n"), n, j.dificuldade);
+	}
+	return 0;
+}
+
+DWORD WINAPI thread_write(LPVOID data) {
+	DWORD n;
+	TCHAR buf[256];
+	BOOL ret;
+	//overlaped I/O
+	HANDLE IOReady;
+	OVERLAPPED Ov;
+	Command c;
+	IOReady = CreateEvent(NULL, TRUE, FALSE, NULL);
+	do {
+
+		_tprintf(TEXT("COMMAND: "));
+		_tprintf(TEXT("Insira id comando: "));
+		_tscanf_s(TEXT("%d"), &c.id);
+		_tprintf(TEXT("\nInsira valor do comando: "));
+		_tscanf_s(TEXT("%d"), &c.cmd);
+
+
+		ZeroMemory(&Ov, sizeof(Ov));
+		ResetEvent(IOReady);
+		Ov.hEvent = IOReady;
+
+		WriteFile(hPipe, &c, sizeof(Command), &n, &Ov);
+
+		WaitForSingleObject(IOReady, INFINITE);
+		ret = GetOverlappedResult(hPipe, &Ov, &n, FALSE);
+
+		if (!ret) {
+			_tprintf(TEXT("[ERRO] Escrever no pipe!\n"));
+			exit(-1);
+		}
+		_tprintf(TEXT("[DEBUG] Enviei %d bytes ao gateway...\n"), n);
+	} while (_tcscmp(buf, TEXT("fim")));
+
+
+
+	return 0;
+}
--- Space Invaders/Gateway/Source.cpp
+++ Space Invaders/Gateway/Source.cpp
@@ -1,15 +1,19 @@
 #include "../Space Invaders/utils.h"
 
+#define PIPE_NAME TEXT("\\\\.\\pipe\\GamePipe")
+
 BOOL(*OpenGame)(void);
 BOOL(*OpenBuff)(void);
 Command(*GetMSG)();
 BOOL(*wrtMSG)(Command);
 pBuff(*rbuff)();
-pJogo(*getGame)();
+Jogo(*getGame)();
 
-HANDLE eGameAcess, hGameUpdateThread;
+HANDLE eGameAcess, hGameUpdateThread, hPlayer[5], hConnectThread;
 //THREADs
 DWORD WINAPI GameUpdateThread(LPVOID params);
+DWORD WINAPI thread_read(LPVOID data);
+DWORD WINAPI connect_Thread(LPVOID data);
 
 int _tmain(int argc, LPTSTR argv[]) {
 #ifdef UNICODE
@@ -29,19 +33,24 @@ int _tmain(int argc, LPTSTR argv[]) {
 	//DLL functions start
 	OpenGame = (BOOL(*)())GetProcAddress(hDLL, "openGameMemory");
 	OpenBuff = (BOOL(*)())GetProcAddress(hDLL, "openBuffer");
-	getGame= (pJogo(*)())GetProcAddress(hDLL, "getGame");
+	getGame = (Jogo(*)())GetProcAddress(hDLL, "getGame");
 	wrtMSG = (BOOL(*)(Command))GetProcAddress(hDLL, "WriteBuffer");
 	rbuff = (pBuff(*)())GetProcAddress(hDLL, "returnBuff");
 
 	OpenGame(); //carrega jogo da memoria
 	OpenBuff(); //carrega buffer da memoria
 
+	hConnectThread = CreateThread(NULL, 0, connect_Thread, NULL, 0, 0);
+	if (hConnectThread == NULL) {
+		_tprintf(TEXT("[(DEBUG)Thread:Erro-> Error starting game update thread\n %d  \n"), GetLastError());
+		return 0;
+	}
+
 	hGameUpdateThread = CreateThread(NULL, 0, GameUpdateThread, NULL, 0, 0);
 	if (hGameUpdateThread == NULL) {
 		_tprintf(TEXT("[(DEBUG)Thread:Erro-> Error starting game update thread\n %d  \n"), GetLastError());
 		return 0;
 	}
-
 
 	Command cmd;
 
@@ -56,34 +65,135 @@ int _tmain(int argc, LPTSTR argv[]) {
 			_tprintf(TEXT("Erro: não foi possível escrever do buffer -> %d\n"), GetLastError());
 			return 0;
 		}
-		
+
 	} while (1);
-	_tprintf(TEXT("FIM gat\n"));
+	
+
+	WaitForSingleObject(hGameUpdateThread, INFINITE);
 	return 0;
+}
+
+void broadcast(Jogo gamedata) {
+	OVERLAPPED Ov;
+	HANDLE IOReady = CreateEvent(NULL, TRUE, FALSE, NULL);
+	BOOL ret;
+	DWORD n;
+
+	for (int i = 0; i < 5; i++) {
+		if (hPlayer[i] != 0) {
+
+			ZeroMemory(&Ov, sizeof(Ov));
+			ResetEvent(IOReady);
+			Ov.hEvent = IOReady;
+
+			ret = WriteFile(hPlayer[i], &gamedata, sizeof(Jogo), &n, &Ov);
+
+			WaitForSingleObject(IOReady, INFINITE);
+
+			GetOverlappedResult(hPlayer[i], &Ov, &n, FALSE);
+
+			if (n < sizeof(Jogo)) {
+				_tprintf(TEXT("[ERRO] Escrever no pipe! (WriteFile)\n"));
+				exit(-1);
+			}
+			else {
+				_tprintf(TEXT("[GATEWAY] Enviei %d bytes ao leitor...(WriteFile)\n"), n);
+			}
+		}
+	}
+
 }
 
 DWORD WINAPI GameUpdateThread(LPVOID params) {
 	eGameAcess = OpenEvent(EVENT_ALL_ACCESS, TRUE, _T("GameUpdateEvent"));
-	pJogo gamedata=NULL;
-	
-	do{
+	Jogo gamedata;
+
+	do {
 		WaitForSingleObject(eGameAcess, INFINITE);
 		_tprintf(TEXT("GameUpdateThread\n"));
 
-		gamedata=getGame();
-		
-		//envia actualização a todos os players
-		_tprintf(TEXT("Read Gamedata, dif= %d\n"), gamedata->dificuldade);
-		_tprintf(TEXT("Read Gamedata, nnavesnormais= %d\n"), gamedata->nNavesNormais);
-		_tprintf(TEXT("Read Gamedata, nnavesesquivas= %d\n"), gamedata->nNavesEsquivas);
-		_tprintf(TEXT("Read Gamedata, navenormalX= %d\n"), gamedata->navesnormais[0].e.x);
-	
+		gamedata = getGame();
 
+		broadcast(gamedata);//envia actualização a todos os players
 		
+		_tprintf(TEXT("Read Gamedata, navenormalX= %d\n"), gamedata.navesnormais[14].e.x);
+		_tprintf(TEXT("Read Gamedata, navenormalY= %d\n"), gamedata.navesnormais[14].e.y);
+
+
+
 	} while (1);
 
 	_tprintf(TEXT("FIM GameUpdateThread\n"));
 	return 0;
 
-	
+
+}
+
+DWORD WINAPI connect_Thread(LPVOID data) {
+	//THREAD T1
+	int i;
+	HANDLE p, hT;
+
+	for (int i = 0; i < 5; i++)
+		hPlayer[i] = NULL;
+
+
+	while (1) {
+		_tprintf(TEXT("[DEBUG] Criar uma cópia do pipe '%s' ... (CreateNamedPipe)\n"), PIPE_NAME);
+		p = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED, PIPE_WAIT | PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE, PIPE_UNLIMITED_INSTANCES, sizeof(Jogo), sizeof(Command), 1000, NULL);
+		if (p == INVALID_HANDLE_VALUE) { _tprintf(TEXT("[ERRO] criar pipe! (CreateNamedPipe\n")); exit(-1); }
+
+
+		_tprintf(TEXT("[DEBUG] Esperar ligação de um cliente...(ConnectNamedPipe)\n"));
+		if (!ConnectNamedPipe(p, NULL)) {
+			_tprintf(TEXT("[ERRO] Ligação ao cliente! (ConnectNamedPipe\n"));
+			exit(-1);
+		}
+
+		for (i = 0; i < 5; i++) {
+			if (hPlayer[i] == NULL) {
+				hPlayer[i] = p;
+				_tprintf(TEXT("[CONNECTED] Ligação ao cliente! (ConnectNamedPipe) \n"));
+				break;
+			}
+
+		}
+
+		//thread para tratar pedidos do pipe
+		hT = CreateThread(NULL, 0, thread_read, (LPVOID)p, 0, 0);
+
+
+	}
+	//FIM THREAD
+	return 0;
+}
+
+DWORD WINAPI thread_read(LPVOID data) {
+	Command c;
+	DWORD n;
+	BOOL ret;
+	HANDLE hPipeL = (HANDLE)data;
+	HANDLE IOReady;
+
+
+	OVERLAPPED Ov;
+	IOReady = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	while (1) {
+		ZeroMemory(&Ov, sizeof(Ov));
+		ResetEvent(IOReady);
+		Ov.hEvent = IOReady;
+
+		ReadFile(hPipeL, &c, sizeof(Command), &n, &Ov);
+		WaitForSingleObject(IOReady, INFINITE);
+		ret = GetOverlappedResult(hPipeL, &Ov, &n, FALSE);
+
+		if (!ret || !n) {
+			_tprintf(TEXT("[ERRO] %d %d... (ReadFile)\n"), ret, n);
+			break;
+		}
+		_tprintf(TEXT("[GATEWAY] Recebi %d bytes, cmdnum: '%d'... (ReadFile)\n"), n, c.cmd);
+	}
+
+	return 0;
 }
